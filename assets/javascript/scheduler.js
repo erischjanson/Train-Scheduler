f22f2df6da4f1b2510d7023380f5bc7ae883747f
Elizabeth <erischjanson@gmail.com>
2018-01-08 13:53:37 -0600

    firebase working

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,42 @@
+
+  var config = {
+    apiKey: "AIzaSyBFa95cQyPmC3muTiVEzQVfb_Qme6zupb4",
+    authDomain: "train-a0de9.firebaseapp.com",
+    databaseURL: "https://train-a0de9.firebaseio.com",
+    projectId: "train-a0de9",
+    storageBucket: "",
+    messagingSenderId: "688298807206"
+  };
+
+  firebase.initializeApp(config);
+
+var database=firebase.database();
+
+database.ref().on("value", function(snapshot){
+	//console.log(snapshot.val());
+	train = snapshot.val().trainName;
+	destination = snapshot.val().destinationCity;
+	//replace text of html element
+	//console.log(train);
+	//console.log(destination);
+
+})
+
+
+$("#submitButton").on("click", function(event){
+	
+	event.preventDefault();
+	//will need to creat an array to hold all trains, etc
+	//use a table in html for the schedule
+
+	var train = $("#trainName").val();
+	var destination = $("#destination").val();
+	//console.log(train);
+	//console.log(destination);
+	database.ref().set({
+		trainName: train,
+		destinationCity: destination
+
+	});
+
+})
