1c91f6c3faed71a96b9b977f65bb793ceb500b65
Elizabeth <erischjanson@gmail.com>
2018-01-10 17:04:37 -0600

    updated element names

--- assets/javascript/scheduler.js
+++ assets/javascript/scheduler.js
@@ -15,11 +15,9 @@ var database=firebase.database();
 database.ref().on("value", function(snapshot){
 	//console.log(snapshot.val());
 	train = snapshot.val().trainName;
+	console.log(train);
 	destination = snapshot.val().destinationCity;
 	//replace text of html element
-	//console.log(train);
-	//console.log(destination);
-
 })
 
 
@@ -29,15 +27,29 @@ $("#submitButton").on("click", function(event){
 	//will need to creat an array to hold all trains, etc
 	//use a table in html for the schedule
 
-	var train = $("#trainName").val();
-	var destination = $("#destination").val();
+	var train = $("#formTrainName").val();
+	var destination = $("#formDestination").val();
+	var firstTrain = $("#formFirstTrainTime").val();
+	var frequency = $("#formFrequency").val();
 	//console.log(train);
 	//console.log(destination);
-	database.ref().set({
-		trainName: train,
-		destinationCity: destination
+	//database.ref().set({
+		//trainName: train,
+		//destinationCity: destination
+
+	//});
+
+	database.ref().push({
+		trainName:train,
+		destinationCity:destination,
+		firstTrainTime:firstTrain,
+		frequencyOfTrain:frequency
+
 
 	});
+		console.log(train);
+		console.log(destination);
+
 
 })
 
